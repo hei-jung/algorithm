013d3a50d51a120e3ef6772f040a4e0fb2180fd0
hei-jung <heijung.jang@hotmail.com>
2022-08-24 14:14:21 +0900

    faster

--- leetcode/leetcode_no_1254.py
+++ leetcode/leetcode_no_1254.py
@@ -6,6 +6,8 @@ class Solution(object):
         def dfs(i, j):
             grid[i][j] = -1
             flag = True
+            if i == 0 or i == m - 1 or j == 0 or j == n - 1:
+                flag = False
             if i - 1 >= 0 and grid[i - 1][j] == 0:
                 flag &= dfs(i - 1, j)
             if i + 1 < m and grid[i + 1][j] == 0:
@@ -14,13 +16,9 @@ class Solution(object):
                 flag &= dfs(i, j - 1)
             if j + 1 < n and grid[i][j + 1] == 0:
                 flag &= dfs(i, j + 1)
-            if i == 0 or i == m - 1 or j == 0 or j == n - 1:
-                flag = False
             return flag
 
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 0 and dfs(i, j):
                     count += 1
-
-        return count
