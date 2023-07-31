cbdc97d2cf3a434be468191ac97241439d1c981c
hei-jung <heijung.jang@hotmail.com>
2023-07-31 18:21:03 +0900

    230731 boj 14889 스타트와 링크

---
+++ boj/boj_14889.py
@@ -0,0 +1,37 @@
+n = int(input())
+s = []
+for _ in range(n):
+    s.append(list(map(int, input().split())))
+
+ans = [1000000000]
+
+visited = [0] * n  # 탐색 여부
+
+
+def dfs(x, cnt):
+    if cnt == n // 2:
+        start = 0
+        link = 0
+        for i in range(n - 1):
+            for j in range(i + 1, n):
+                if visited[i] == 1 and visited[j] == 1:
+                    start += s[i][j]
+                    start += s[j][i]
+                elif visited[i] == 0 and visited[j] == 0:
+                    link += s[i][j]
+                    link += s[j][i]
+        ans[0] = min(ans[0], abs(start - link))
+        if ans[0] == 0:
+            print(0)
+            exit(0)
+        return
+    for i in range(x, n):
+        if visited[i] != 1:
+            visited[i] = 1
+            dfs(i + 1, cnt + 1)
+            visited[i] = 0
+
+
+dfs(0, 0)
+
+print(ans[0])
